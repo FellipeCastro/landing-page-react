74cd535890268adbd414d271ee9458e72cb518c0
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-29 09:13:59 -0300

    Home Finalizada

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,17 @@
+import {Link} from 'react-router-dom';
+import styles from './Home.module.css';
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <div className={styles.text_container}>
+                <h2>Cursos de programação, do <span>zero ao profissional</span>, para iniciar ou evoluir a sua carreira como programador!</h2>
+                <Link to="/cursos">Conheça nossos cursos</Link>
+            </div>
+            <div className={styles.img_container}>
+                <img src="../img/Innovation-amico.svg" alt="Cursos Online"/>
+            </div>
+        </section>
     )
 }
 
